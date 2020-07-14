ab0274aa0c512184ce81d6c7e72c57b9acfd67af
yangchuanchao <yangchuanchao@orientsec.com.cn>
2020-07-14 15:21:08 +0800

    - 通过 HttpServer 方式调用

---
+++ Tushare_test/main/server_test/httpserver_test.py
@@ -0,0 +1,23 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+from main.quantization.volumn_acc import Quantization
+
+data = {'result': 'this is a test'}
+host = ('localhost', 8888)
+
+
+class Request(BaseHTTPRequestHandler):
+    def do_GET(self):
+        quant = Quantization()
+        quant.calc_mock()
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Request)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
