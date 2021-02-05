aae35bffcda14b30133a05fb7bb4367315a2463e
chaifeng <fenglingne@gmail.com>
2021-02-05 17:23:32 +0800

    根据系统创建文件夹

--- Wind_data/utility/folder_utility.py
+++ Wind_data/utility/folder_utility.py
@@ -1,8 +1,18 @@
 import os
+import platform
 
 
 # 创建文件目录结构
 def create_folder(folder_name):
+    if platform.system() == "Windows":
+        folder_name = 'D:/' + folder_name
+    else:
+        folder_name = folder_name
+
     folder_name_exist = os.path.exists(folder_name)
+
     if not folder_name_exist:
         os.makedirs(folder_name)
+
+
+create_folder('./pictures/aaa')
