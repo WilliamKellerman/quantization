73555337c6426ae7f722ca8e068e076300abee27
yangchuanchao <yangchuanchao@orientsec.com.cn>
2021-02-07 22:14:41 +0800

    Wind heavy stock hold ~ 通过excel读取基金DF

--- Wind_data/utility/excel_utility.py
+++ Wind_data/utility/excel_utility.py
@@ -19,3 +19,16 @@ def append_to_new_sheet(data, wb_file, sheet_name):
     # 将data写入writer
     data.to_excel(writer, sheet_name=sheet_name, index=False)
     writer.save()
+
+
+def read_xls():
+    sheet = pd.read_excel('../application/data/2021产品列表.xlsx', sheet_name=1, header=1, skiprows=0)
+    sheet = sheet[sheet['产品类型'].isin(['普通开放式基金', '持有期基金', '定开基金'])]
+
+    # 基金代码int->str, rjust 补足6位，前面填充0
+    sheet['基金代码'] = sheet.apply(lambda row: str(row['产品代码']).rjust(6, '0') + '.OF', axis='columns')
+
+    return sheet
+
+
+read_xls()
