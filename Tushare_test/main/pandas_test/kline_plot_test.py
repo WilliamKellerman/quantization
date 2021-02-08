57fb54a1b264404841a0b00ef2f276fa4ced3516
yangchuanchao <yangchuanchao@orientsec.com.cn>
2021-02-09 01:27:09 +0800

    测试验证画图

--- Tushare_test/main/pandas_test/kline_plot_test.py
+++ Tushare_test/main/pandas_test/kline_plot_test.py
@@ -7,7 +7,7 @@ now = datetime.datetime.today().strftime('%Y%m%d')
 hold = [1232123123, 23123123123, 32334234422, 12842928383]
 
 last = datetime.date(datetime.date.today().year, datetime.date.today().month, 1)-datetime.timedelta(1)
-years = ['20200101', '20200501', '20201001', last]
+years = ['20190101', '20190501', '20191001', '20200131']
 pg_1 = pd.DataFrame({'持仓': hold, '日期': years})
 pg_1.index = pg_1['日期']
 pg_1 = pg_1.rename(index=pd.Timestamp)
@@ -23,11 +23,11 @@ price_df.sort_index(inplace=True)
 price_df.info()
 
 add_plot = mpf.make_addplot(df_m, type='line', secondary_y='auto')
-mpf.plot(price_df, type='candle', volume=True, style='yahoo', addplot=add_plot)
+mpf.plot(price_df, type='candle', volume=True, style='yahoo', addplot=add_plot, show_nontrading=True)
 
 # 验证得出 X轴密度相同，数值不同可以画在一起
 # 问题1：点位不完全正确
-# 目前看起来X轴是由 plot 入参 df 确定， addplot 以现有的X轴按顺序画点
+# 目前看起来X轴是由 plot 入参 df 确定， addplot 以现有的X轴按顺序画点      结论正确
 # 问题2：目前只找到了向前插值，向后插值 api 暂时还没有找到均匀填充的方法
 
 print('end')
