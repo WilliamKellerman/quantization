dc93cdfe7e8301c7068a90d64545e8a2603a98d0
Achui <kidlike@126.com>
2021-02-09 09:32:06 +0800

    验证线性填充

--- Tushare_test/main/pandas_test/kline_plot_test.py
+++ Tushare_test/main/pandas_test/kline_plot_test.py
@@ -7,12 +7,12 @@ now = datetime.datetime.today().strftime('%Y%m%d')
 hold = [1232123123, 23123123123, 32334234422, 12842928383]
 
 last = datetime.date(datetime.date.today().year, datetime.date.today().month, 1)-datetime.timedelta(1)
-years = ['20190101', '20190501', '20191001', '20200131']
+years = ['20190131', '20190531', '20191031', '20200131']
 pg_1 = pd.DataFrame({'持仓': hold, '日期': years})
 pg_1.index = pg_1['日期']
 pg_1 = pg_1.rename(index=pd.Timestamp)
 pg_2 = pg_1['持仓']
-df_m = pg_2.resample('M').ffill()
+series_m = pg_2.resample('M').asfreq().interpolate()
 
 price_df = ts.pro_bar(ts_code='600958.SH', adj='qfq', start_date='20200101', end_date=now, freq='M')
 price_df.index = price_df.trade_date
@@ -22,12 +22,13 @@ price_df.columns = ['close', 'open', 'high', 'low', 'volume']
 price_df.sort_index(inplace=True)
 price_df.info()
 
-add_plot = mpf.make_addplot(df_m, type='line', secondary_y='auto')
-mpf.plot(price_df, type='candle', volume=True, style='yahoo', addplot=add_plot, show_nontrading=True)
+add_plot = mpf.make_addplot(series_m, type='line', secondary_y='auto')
+mpf.plot(price_df, type='candle', volume=True, style='yahoo', addplot=add_plot)
 
 # 验证得出 X轴密度相同，数值不同可以画在一起
 # 问题1：点位不完全正确
 # 目前看起来X轴是由 plot 入参 df 确定， addplot 以现有的X轴按顺序画点      结论正确
 # 问题2：目前只找到了向前插值，向后插值 api 暂时还没有找到均匀填充的方法
+# 目前尝试出可行方案为通过 asfreq() 重采集后获得nan空位，再使用 interpolate() 线性填充，这个方案存在问题，重采集点位固定，如果原数据不在重采集点位内，则会被丢弃。
 
 print('end')
