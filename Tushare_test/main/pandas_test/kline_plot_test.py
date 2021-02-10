f15ed09da4598a5723cbe29779ac6de6998c46a1
Achui <kidlike@126.com>
2021-02-10 14:13:12 +0800

    merge优化保留月线 持仓原始数据起始补0

--- Tushare_test/main/pandas_test/kline_plot_test.py
+++ Tushare_test/main/pandas_test/kline_plot_test.py
@@ -40,31 +40,53 @@ def test_interpolate():
     plt.show()
 
 
+def get_last_quarter(date_s):
+    first_date = datetime.datetime.strptime(date_s[0], '%Y%m%d')
+    quarter = (first_date.month - 1) / 3 + 1
+    if quarter == 1:
+        return datetime.datetime(first_date.year - 1, 12, 31).strftime('%Y%m%d')
+    elif quarter == 2:
+        return datetime.datetime(first_date.year, 3, 31).strftime('%Y%m%d')
+    elif quarter == 3:
+        return datetime.datetime(first_date.year, 6, 30).strftime('%Y%m%d')
+    else:
+        return datetime.datetime(first_date.year, 9, 30).strftime('%Y%m%d')
+
+
 def test_merge_data_get_real_date():
     now = datetime.datetime.today().strftime('%Y%m%d')
     hold = [1232123123, 23123123123, 32334234422, 12842928383]
 
     last = datetime.date(datetime.date.today().year, datetime.date.today().month, 1) - datetime.timedelta(1)
-    date = ['20200131', '20200531', '20201031', '20210131']
+    date = ['20200630', '20200531', '20201031', '20210131']
     pg_1 = pd.DataFrame({'持仓': hold, '日期': date})
+    # 对起始日期上季度数据补0
+    pg_1.sort_values(by='日期')
+    date_s = pg_1['日期']
+    date_need_supply = get_last_quarter(date_s)
+    first_data = {'持仓': 0, '日期': date_need_supply}
+    pg_1.append(first_data, ignore_index=True)
+
     pg_1.index = pg_1['日期']
     pg_1 = pg_1.rename(index=pd.Timestamp)
     pg_2 = pg_1['持仓']
     # TODO: 持仓左侧（或右侧）非0，拟合的函数有缺失，应保证左侧为0，右侧视情况
+    # 已实现，日期列表起始点改为上季度最后一天，补数为0
     series_m = pg_2.resample('D').asfreq().interpolate()
     df_w = pd.DataFrame({'持仓': series_m})
     df_w = df_w.reset_index()
 
-    price_df = ts.pro_bar(ts_code='600958.SH', adj='qfq', start_date='20200101', end_date=now, freq='M')
+    price_df = ts.pro_bar(ts_code='600958.SH', adj='qfq', start_date='20150101', end_date=now, freq='M')
     price_df.drop(columns=['ts_code', 'pre_close', 'change', 'pct_chg', 'amount'], inplace=True)
     price_df.columns = ['trade_date', 'close', 'open', 'high', 'low', 'volume']
     price_df['日期'] = price_df.apply(lambda r: datetime.datetime.strptime(r['trade_date'], '%Y%m%d'), axis='columns')
-    price_df = price_df.reset_index()
 
     # TODO: 用 right join，仅保留每月线，
     #  理由1 日线都是拟合出来的，没有意义, 且占用存储
     #  理由2 price_DF, 持仓_DF, 数据时间跨度不一样，join之后，日线数据和月线数据混合，mplfinance画出的图只会简单数点，不缺分精度
-    df_new = pd.merge(left=df_w, right=price_df, how='outer', on='日期')
+    # 已实现
+    df_new = price_df.merge(df_w, how='left', on='日期')
+    # df_new = pd.merge(left=df_w, right=price_df, how='outer', on='日期')
     df_new.index = df_new['日期']
     df_new = df_new.rename(index=pd.Timestamp)
     df_new = df_new.sort_index(ascending=True)
